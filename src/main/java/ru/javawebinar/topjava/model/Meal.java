bd921bf190605175d6a24e87a4fd2a7edf4267fb
BaranchikovAlex <a.v.baranchikov@gmail.com>
2018-07-25 01:00:14 +0300

    HW04 do Meal and JpaMealRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,15 +1,51 @@
 package ru.javawebinar.topjava.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM meals m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.GET, query = "SELECT m FROM meals m " +
+                "WHERE m.id =:id AND m.user.id =:userId"),
+        @NamedQuery(name = Meal.GET_ALL, query = "SELECT m FROM meals m " +
+                "WHERE m.user.id=:userId " +
+                "ORDER BY m.date_time DESC"),
+        @NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM meals m " +
+                "WHERE m.user.id=:userId  AND m.date_time BETWEEN  :startDate AND :endDate " +
+                "ORDER BY m.date_time DESC")
+})
+
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
-    private final LocalDateTime dateTime;
 
-    private final String description;
+    public static final String DELETE = "Meal.DELETE";
+    public static final String GET = "Meal.GET";
+    public static final String GET_ALL = "Meal.GET_ALL";
+    public static final String GET_BETWEEN = "Meal.GET_BETWEEN";
 
-    private final int calories;
+    @Column(name = "date_time", nullable = false)
+    @Convert(converter = LocalDatePersistenceConverter.class)
+    @NotBlank
+    private LocalDateTime dateTime;
+
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    private String description;
+
+    @Column(name = "calories", nullable = false)
+    @NotBlank
+    private int calories;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Meal() {
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
@@ -40,6 +76,26 @@ public class Meal extends AbstractBaseEntity {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override
